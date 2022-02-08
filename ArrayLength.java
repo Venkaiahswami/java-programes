20388b130352ecc125e9ecaa5b0d388722881d59
Venkaiahswami <89331564+Venkaiahswami@users.noreply.github.com>
2022-02-08 21:15:42 +0530

    Add files via upload

---
+++ ArrayLength.java
@@ -0,0 +1,14 @@
+public class ArrayLength {
+  public static void main(String[] args) {
+    
+    // declare and initialize an array
+    int arr[][] = {{50,60},{70,80},{90,100}};
+    
+    // display array length
+    System.out.print("The length of the given array = ");
+    System.out.println(arr.length);
+    System.out.println("arr[0] length = " + arr[0].length);
+    System.out.println("arr[1] length = " + arr[0].length);
+    System.out.println("arr[2] length = " + arr[0].length);
+  }
+}
