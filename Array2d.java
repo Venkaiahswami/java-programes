032aa8104345c9058e1781bd650f93c49a190ae6
Venkaiahswami <89331564+Venkaiahswami@users.noreply.github.com>
2022-02-08 21:12:15 +0530

    Add files via upload

---
+++ Array2d.java
@@ -0,0 +1,11 @@
+ class Array2d {
+     public static void main(String args[]){
+         int a[][]={{1,2,3,4},{1,4,6,8},{1,4,2,3}};
+         for(int i=0;i<3;i++){
+             for(int j=0;j<4;j++)
+             System.out.print(a[i][j]+"\t");
+         }
+         
+     }
+    
+}
