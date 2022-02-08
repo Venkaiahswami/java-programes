f3420e05c620227d921caeb2a78ebd19a220b8e5
Venkaiahswami <89331564+Venkaiahswami@users.noreply.github.com>
2022-02-08 21:10:58 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,15 @@
+class Array
+{
+    public static void main(String args[])
+    {
+        int marks[]=new int[3];
+        marks[0]=98;
+        marks[1]=95;
+        marks[2]=88;
+        for(int i=0;i<marks.length;i++){
+
+            System.out.println(marks[i]);
+        }
+
+    }
+}
